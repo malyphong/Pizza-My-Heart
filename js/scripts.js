f688a691ca108960882a4348127e2dab3610c66b
Maly Phongsavanh <phongsavanh619@icloud.com>
2018-08-24 11:42:06 -0700

    create variables for each input

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ function Purchaser(name, address) {
   this.name = name;
   this.address = address;
 }
-
 function Order(size, protein, toppings) {
 this.size = size;
 this.protein = protein;
@@ -20,12 +19,24 @@ $(document).ready(function() {
     $(".inputInformation").hide();
     $(".form-group").show();
 
+    var name = $("input.name");
+    var address = $("input.address");
+    var newPurchaser = new Purchaser(name, address);
+
   });
   $(".form-group").submit(function(event) {
     event.preventDefault();
     $(".form-group").hide();
     $(".completedOrder").show();
 
+      var size = $("input[name=size]").val();
+      var protein = $("input[name=protein]").val();
+      var toppings = [];
+      $("input:checkbox[name=toppings]:checked").map(function() {
+        toppings.push($(this).val());
+      var newOrder = new Order(size, protein, toppings);
 
+      console.log(newOrder);
+})
   });
 });
