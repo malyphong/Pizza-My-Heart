925ac22f86371760a053d730a4eadaad2548e6a8
Maly Phongsavanh <phongsavanh619@icloud.com>
2018-08-28 23:58:41 -0700

    show purchaser name and address in order output

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,22 @@
 // Business Logic
-function Purchaser(name, address) {
-  this.name = name;
-  this.address = address;
-
-}
 function Order(size, protein, toppings, price) {
   this.size = size;
   this.protein = protein;
   this.toppings = toppings;
-  this.price = price;
+  this.price = 3;
 }
 
 Order.prototype.sizePrice = function(size) {
-  var pizzaPrice = 3;
-  if (this.size === "small") {
-    this.price = pizzaPrice += 5;
+  if (this.size === "small" && this.toppings >= 2) {
+    this.price += 5;
   } else if (this.size === "medium") {
-    this.price = pizzaPrice += 10;
+    this.price += 10;
   } else if (this.size === "large") {
-    this.price = pizzaPrice += 13;
-  }
+    this.price += 13;
+  } else {
   return this.price;
+  }
 }
-
 // User Interface Logic
 $(document).ready(function() {
   $(".inputInformation").submit(function(event) {
@@ -30,8 +24,12 @@ $(document).ready(function() {
     $(".inputInformation").hide();
     $(".form-group").show();
 
-    var name = $("input.name");
-    var address = $("input.address");
+    var name = $("input#name").val();
+    var address = $("input#address").val();
+    console.log(name);
+
+    $("#purchaserName").text(name);
+    $("#purchaserAddress").text(address);
 
   });
   $(".form-group").submit(function(event) {
@@ -41,11 +39,15 @@ $(document).ready(function() {
 
     var size = $("input[name=size]:checked").val();
     var protein = $("input[name=protein]:checked").val();
+    var price = 3;
     var toppings = [];
     $("input[name=toppings]:checked").map(function() {
       toppings.push($(this).val());
-      var newOrder = new Order (size, protein, toppings);
+      var newOrder = new Order(size, protein, toppings, price);
       console.log(newOrder);
+
+      $(".finishOrder").append(newOrder)
+
 
     })
   });
